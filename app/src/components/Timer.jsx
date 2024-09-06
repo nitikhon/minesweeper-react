10567a0be0b2353114b1c4590c7f6aae98a09b2d
Nitikhon Chantatham <taeza530@gmail.com>
2024-09-06 14:28:43 +0700

    fix Timer not work after start the new game

--- app/src/components/Timer.jsx
+++ app/src/components/Timer.jsx
@@ -1,25 +1,17 @@
 import React, { useState, useEffect } from 'react';
 
 export default function Stopwatch(props) {
-    const [isActive, setIsActive] = useState(true);
-
     useEffect(() => {
-        if (props.gameOver){
-            setIsActive(prevState => !prevState);
-        }
-
         let interval = null;
 
-        if (isActive) {
+        if (!props.gameOver){
             interval = setInterval(() => {
                 props.setTime((prevTime) => prevTime + 10); // Increment by 10ms for more accuracy
             }, 10);
-        } else if (!isActive && props.time !== 0) {
-            clearInterval(interval);
         }
 
         return () => clearInterval(interval);
-    }, [isActive, props.gameOver]);
+    }, [props.gameOver]);
 
     return (
         <div>
